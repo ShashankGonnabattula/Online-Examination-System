4f02c26c6e3025f208f74153f4ba56bf3a4f31af
ShashankGonnabattula <venkat192333@gmail.com>
2025-09-24 09:50:31 +0530

    V1.0.1

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,11 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <BrowserRouter basename="/Online-Examination-System">
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+</BrowserRouter>
+
   </React.StrictMode>,
 )
